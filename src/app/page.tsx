4af251fcdb835e554d7b59394fe082b9dd782c92
rodrigo <javierluis15975@hotmail.com>
2025-01-19 04:31:04 -0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import PageHome from '@/modules/home/pages/page-home'
 import PageCard from '@/modules/products/pages/pageCard'
 import BrowseStylesPage from '@/modules/browse-styles/pages/browseStylesPage'
+
 export default function Home() {
   return (
     <>
