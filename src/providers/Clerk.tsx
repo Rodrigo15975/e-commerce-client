b562c644e266e59f4c0bbcf198dd365fdcfeaba2
rodrigo <javierluis15975@hotmail.com>
2025-01-20 02:44:32 -0500

    update deploy-dev.yml and Clerk.tsx

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -3,6 +3,9 @@ import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
   const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+  console.log({
+    publishableKey,
+  })
 
   return (
     <>
