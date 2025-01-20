069d9785e00e696ff29e2a4e6471b18f458f032b
rodrigo <javierluis15975@hotmail.com>
2025-01-20 02:40:02 -0500

    create 1 file and update 3 files

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -2,16 +2,11 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
-  const publishableKey =
-    process.env.NODE_ENV === `production`
-      ? process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-      : process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
 
   return (
     <>
-      <ClerkProvider signInUrl="/sign-in" publishableKey={publishableKey}>
-        {children}
-      </ClerkProvider>
+      <ClerkProvider publishableKey={publishableKey}>{children}</ClerkProvider>
     </>
   )
 }
