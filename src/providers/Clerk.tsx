58d0232134967e09e79cc679d1751de99f2c3845
rodrigo <javierluis15975@hotmail.com>
2025-01-20 02:52:22 -0500

    update Clerk.tsx

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -2,20 +2,10 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
-  // const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-  // console.log({
-  //   publishableKey,
-  // })
-
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
   return (
     <>
-      <ClerkProvider
-        publishableKey={
-          'pk_live_Y2xlcmsuZDJsMm94cm9lcXFldWsuYW1wbGlmeWFwcC5jb20k'
-        }
-      >
-        {children}
-      </ClerkProvider>
+      <ClerkProvider publishableKey={publishableKey}>{children}</ClerkProvider>
     </>
   )
 }
