9d36b8420ab1b3bdb28767e60b47c5c8fd02166a
rodrigo <javierluis15975@hotmail.com>
2025-01-20 02:54:43 -0500

    update deploy-dev.yml and Clerk.tsx

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -2,10 +2,9 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
   return (
     <>
-      <ClerkProvider publishableKey={publishableKey}>{children}</ClerkProvider>
+      <ClerkProvider>{children}</ClerkProvider>
     </>
   )
 }
