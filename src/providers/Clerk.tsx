09f0d6c4fb4f461ea02ca8577c8380983dcb99ac
rodrigo <javierluis15975@hotmail.com>
2025-01-20 02:46:16 -0500

    fix: update ClerkProvider to use a hardcoded publishable key

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -2,14 +2,20 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-  console.log({
-    publishableKey,
-  })
+  // const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+  // console.log({
+  //   publishableKey,
+  // })
 
   return (
     <>
-      <ClerkProvider publishableKey={publishableKey}>{children}</ClerkProvider>
+      <ClerkProvider
+        publishableKey={
+          'pk_live_Y2xlcmsuZDJsMm94cm9lcXFldWsuYW1wbGlmeWFwcC5jb20k'
+        }
+      >
+        {children}
+      </ClerkProvider>
     </>
   )
 }
