2fcfb31910e5da5ce6e709a13a7fe9ab4e1ac751
rodrigo <javierluis15975@hotmail.com>
2025-01-20 03:24:30 -0500

    refactor: remove hardcoded publishable key from ClerkProvider

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -1,11 +1,11 @@
+'use client'
 import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
-  const publishKey = 'pk_test_bWFzc2l2ZS1ndXBweS05Ni5jbGVyay5hY2NvdW50cy5kZXYk'
   return (
     <>
-      <ClerkProvider publishableKey={publishKey}>{children}</ClerkProvider>
+      <ClerkProvider>{children}</ClerkProvider>
     </>
   )
 }
