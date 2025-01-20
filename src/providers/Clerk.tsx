2aa3a7737f9defc5bc73e83f5126b99ca04575ad
rodrigo <javierluis15975@hotmail.com>
2025-01-20 03:00:35 -0500

    fix: pass publishable key from environment to ClerkProvider

--- src/providers/Clerk.tsx
+++ src/providers/Clerk.tsx
@@ -2,9 +2,10 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { FC, PropsWithChildren } from 'react'
 
 const Clerk: FC<PropsWithChildren> = ({ children }) => {
+  const publishKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
   return (
     <>
-      <ClerkProvider>{children}</ClerkProvider>
+      <ClerkProvider publishableKey={publishKey}>{children}</ClerkProvider>
     </>
   )
 }
