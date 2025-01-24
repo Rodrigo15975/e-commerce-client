a9b574e692945f20c278ccc9c74cf6a211950c7c
rodrigo <javierluis15975@hotmail.com>
2025-01-24 04:10:47 -0500

    update 1 file and create 1 file

--- src/modules/products/components/products-review.tsx
+++ src/modules/products/components/products-review.tsx
@@ -14,45 +14,7 @@ import { useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 import { FaGoogle } from 'react-icons/fa'
 import ProductNewReview from './product-new-review'
-
-const testimonials = [
-  {
-    name: 'Sarah M.',
-    verified: true,
-    rating: 4,
-    text: "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
-  },
-  {
-    name: 'Alex K.',
-    verified: true,
-    rating: 4,
-    text: 'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.',
-  },
-  {
-    name: 'James L.',
-    verified: true,
-    rating: 5,
-    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
-  },
-  {
-    name: 'James L.',
-    verified: true,
-    rating: 4,
-    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
-  },
-  {
-    name: 'James L.',
-    verified: true,
-    rating: 3,
-    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
-  },
-  {
-    name: 'James L.',
-    verified: true,
-    rating: 5,
-    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
-  },
-]
+import { testimonials } from '../constants/testimonials'
 
 type Props = {
   post: Post[]
---
+++ src/modules/products/constants/testimonials.ts
@@ -0,0 +1,38 @@
+export const testimonials = [
+  {
+    name: 'Sarah M.',
+    verified: true,
+    rating: 4,
+    text: "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+  },
+  {
+    name: 'Alex K.',
+    verified: true,
+    rating: 4,
+    text: 'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.',
+  },
+  {
+    name: 'James L.',
+    verified: true,
+    rating: 5,
+    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+  },
+  {
+    name: 'James L.',
+    verified: true,
+    rating: 4,
+    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+  },
+  {
+    name: 'James L.',
+    verified: true,
+    rating: 3,
+    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+  },
+  {
+    name: 'James L.',
+    verified: true,
+    rating: 5,
+    text: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+  },
+]
